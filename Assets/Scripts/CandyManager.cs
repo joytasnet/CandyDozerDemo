184118ce2cb2dc3a24b164afc61092c08f25e143
mjpurin <mjpurin@gmail.com>
2024-11-05 13:15:32 +0900

    P208

--- Assets/Scripts/CandyManager.cs
+++ Assets/Scripts/CandyManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class CandyManager : MonoBehaviour
 {
     const int DefaultCandyAmount = 30;
+    const int RecoverySeconds = 10;
 
     public int candy=DefaultCandyAmount;
+    int counter;
 
     public void ConsumeCandy(){
         if(candy > 0 ) candy--;
@@ -24,7 +26,21 @@ public class CandyManager : MonoBehaviour
         GUI.matrix=Matrix4x4.Scale(Vector3.one*2);
         GUI.color=Color.black;
 
-        string label ="Candy :" + candy;
+        string label =$"Candy :{candy}";
+        if (counter > 0) label=$"{label} ({counter}s)";
         GUI.Label(new Rect(20,20,100,30),label);
+    }
+    void Update(){
+        if(candy < DefaultCandyAmount && counter <= 0){
+            StartCoroutine(RecoverCandy());
+        }
+    }
+    IEnumerator RecoverCandy(){
+        counter = RecoverySeconds;
+        while(counter > 0){
+            yield return new WaitForSeconds(1.0f);
+            counter--;
+        }
+        candy++;
     }
 }
